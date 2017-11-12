c14afd4030dadb25704c5c8ec2df8265c467f24b
Nikita Kazhamiaka <nikiti498@gmail.com>
2017-11-12 14:58:56 +0300

    Initial commit

---
+++ SteamTradeBot/SteamTradeBot/SteamTradeBot/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SteamTradeBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenBrowser = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.Stop_selling = new System.Windows.Forms.Button();
+            this.Start_selling = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PriceChange = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.PriceChange)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenBrowser
+            // 
+            this.OpenBrowser.Location = new System.Drawing.Point(12, 12);
+            this.OpenBrowser.Name = "OpenBrowser";
+            this.OpenBrowser.Size = new System.Drawing.Size(216, 33);
+            this.OpenBrowser.TabIndex = 0;
+            this.OpenBrowser.Text = "Открыть Steam в браузере";
+            this.OpenBrowser.UseVisualStyleBackColor = true;
+            this.OpenBrowser.Click += new System.EventHandler(this.OpenBrowser_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(523, 206);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(90, 23);
+            this.Close.TabIndex = 1;
+            this.Close.Text = "Закрыть";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Stop_selling
+            // 
+            this.Stop_selling.Location = new System.Drawing.Point(12, 193);
+            this.Stop_selling.Name = "Stop_selling";
+            this.Stop_selling.Size = new System.Drawing.Size(231, 48);
+            this.Stop_selling.TabIndex = 2;
+            this.Stop_selling.Text = "Остановить продажу";
+            this.Stop_selling.UseVisualStyleBackColor = true;
+            this.Stop_selling.Click += new System.EventHandler(this.Stop_selling_Click);
+            // 
+            // Start_selling
+            // 
+            this.Start_selling.Location = new System.Drawing.Point(12, 128);
+            this.Start_selling.Name = "Start_selling";
+            this.Start_selling.Size = new System.Drawing.Size(231, 48);
+            this.Start_selling.TabIndex = 3;
+            this.Start_selling.Text = "Начать продажу";
+            this.Start_selling.UseVisualStyleBackColor = true;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Sell);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Добавить к минимальной цене в валюте установленной стимом";
+            // 
+            // PriceChange
+            // 
+            this.PriceChange.DecimalPlaces = 2;
+            this.PriceChange.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.PriceChange.Location = new System.Drawing.Point(493, 71);
+            this.PriceChange.Name = "PriceChange";
+            this.PriceChange.Size = new System.Drawing.Size(120, 22);
+            this.PriceChange.TabIndex = 5;
+            this.PriceChange.ValueChanged += new System.EventHandler(this.PriceChange_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 253);
+            this.Controls.Add(this.PriceChange);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Start_selling);
+            this.Controls.Add(this.Stop_selling);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.OpenBrowser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PriceChange)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenBrowser;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Button Stop_selling;
+        private System.Windows.Forms.Button Start_selling;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown PriceChange;
+    }
+}
+
---
+++ SteamTradeBot/SteamTradeBot/SteamTradeBot/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace SteamTradeBot
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver Browser;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sell(object sender, DoWorkEventArgs e)
+       {
+
+
+           IWebElement SteamContainer = Browser.FindElement(By.ClassName("inventory_ctn"));
+
+           int CardsProcessed = 0;
+           bool FirstSell = true;
+
+
+           List<IWebElement> Items = SteamContainer.FindElements(By.ClassName("itemHolder")).ToList();
+           for (int i = 0; i < Items.Count; i++)
+           {
+
+               if (backgroundWorker1.CancellationPending) return;
+
+               if (Items[i].GetAttribute("style") == "")
+               {
+                   System.Threading.Thread.Sleep(2000);
+                   Items[i].Click();
+
+                   String DivText = "";
+                   IWebElement SellBtn = null;
+
+                   System.Threading.Thread.Sleep(1000);
+                   List<IWebElement> SellDivs = Browser.FindElements(By.ClassName("item_market_actions")).ToList();
+                   for (int j = 0; j < SellDivs.Count; j++)
+                   {
+                       if (SellDivs[j].Displayed)
+                       {
+                           DivText = SellDivs[j].Text;
+                           SellBtn = SellDivs[j].FindElement(By.CssSelector(".item_market_action_button.item_market_action_button_green"));
+                       }
+                   }
+
+                   String priceStr = System.Text.RegularExpressions.Regex.Match(DivText, @"[0-9]+\,?[0-9]*").Value;
+                   float Price = Single.Parse(priceStr) + (float)PriceChange.Value;
+
+
+                   Actions SellClick = new Actions(Browser);
+                   SellClick.MoveToElement(SellBtn).Click().Perform();
+
+                   //SellBtn.Click();
+                   System.Threading.Thread.Sleep(2000);
+
+                   if (FirstSell)
+                   {
+                       Browser.FindElement(By.Id("market_sell_dialog_accept_ssa")).Click();
+                       FirstSell = false;
+                   }
+
+                   Browser.FindElement(By.Id("market_sell_buyercurrency_input")).SendKeys(Price.ToString());
+                   System.Threading.Thread.Sleep(1000);
+                   Browser.FindElement(By.Id("market_sell_dialog_accept")).Click();
+                   System.Threading.Thread.Sleep(1000);
+                   Browser.FindElement(By.Id("market_sell_dialog_ok")).Click();
+
+                   System.Threading.Thread.Sleep(3000);
+
+
+
+
+                   Browser.FindElement(By.CssSelector(".newmodal_buttons .btn_grey_white_innerfade.btn_medium")).Click();
+
+                   CardsProcessed++;
+                   if (CardsProcessed == 25)
+                   {
+                       CardsProcessed = 0;
+                       Browser.FindElement(By.Id("pagebtn_next")).Click();
+                       System.Threading.Thread.Sleep(2000);
+                   }
+               }
+           }
+
+
+
+       }
+        private void OpenBrowser_Click(object sender, EventArgs e)
+        {
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(10);
+            Browser.Manage().Window.Maximize();
+            Browser.Navigate().GoToUrl("http://store.steampowered.com");
+           
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Browser.Quit();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void Stop_selling_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void PriceChange_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
